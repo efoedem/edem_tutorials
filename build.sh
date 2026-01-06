3723876ad8aaf44de6413cd99d39fd1f9248c953
efoedem <kwakuedemhlodzeprince@gmail.com>
2026-01-06 19:29:01 +0000

    Fixed build script order

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
-pip install --upgrade pip
+# 1. Upgrade pip FIRST so it can handle modern packages
+python -m pip install --upgrade pip
+
+# 2. Install your project requirements
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
-
-# Collect static files
+# 3. Collect static files (Cloudinary needs this)
 python manage.py collectstatic --no-input
+
+# 4. Run database migrations
+python manage.py migrate
