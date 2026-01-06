66d988cdd09012d3aaa6c80ad30cd7fa2cba4954
efoedem <kwakuedemhlodzeprince@gmail.com>
2026-01-06 18:00:45 +0000

    Update models and apply migrations

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,20 +1,24 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.conf import settings
+
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    # Cloudinary will use this field to store your images permanently
     thumbnail = models.ImageField(upload_to='course_thumbnails/', blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    # Adding price so it shows up on your cards (e.g., 200ghc)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
     teacher = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='courses_taught'
     )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+# This will create a superuser automatically if it doesn't exist
+python manage.py createsuperuser --noinput || true
